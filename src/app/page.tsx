38ef74f8008fe6546ef56ab1be21d8d480491bb5
hendra setiawan <hendra.setiawan@primus.co.id>
2025-04-16 18:09:58 +0700

    update favicon, title, description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import AboutUs from "@/components/About";
 import OurClients from "@/components/Clients";
 import ContactUs from "@/components/ContactUs";
 import HeroSection from "@/components/Hero";
-import Navbar from "@/components/Navbar";
 import OurPrincipals from "@/components/OurPrincipals";
 import ServicesSolutions from "@/components/ServicesSolutions";
 
@@ -10,8 +9,6 @@ export default function Home() {
   return (
     <>
       {/* <ParallaxImage /> */}
-
-      <Navbar />
 
       <HeroSection />
 
